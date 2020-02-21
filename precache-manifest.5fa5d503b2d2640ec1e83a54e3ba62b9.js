7f70f2c00a8cf993b5f3f5234eca8d0cc9153212
Luis Roman <louisroman133@gmail.com>
2020-02-21 09:52:15 -0500

    Updates

---
+++ precache-manifest.5fa5d503b2d2640ec1e83a54e3ba62b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "267e20ea282fe5eab9c9b8fa9c707f02",
+    "url": "/ninjas/index.html"
+  },
+  {
+    "revision": "67fd477d9047b5c0a0c9",
+    "url": "/ninjas/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e3c2a59f332e3230b419",
+    "url": "/ninjas/static/js/2.20173c48.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ninjas/static/js/2.20173c48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67fd477d9047b5c0a0c9",
+    "url": "/ninjas/static/js/main.d31006e7.chunk.js"
+  },
+  {
+    "revision": "e25fbf54dcfda0e6a259",
+    "url": "/ninjas/static/js/runtime-main.b33b9a8c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ninjas/static/media/logo.5d5d9eef.svg"
+  }
+]);
